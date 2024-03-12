9ca191a9ce8432caa6f449da26a0c8efe1c296e8
Peizhen Liao <peizhenliao23@gmail.com>
2024-03-12 00:20:30 -0400

    Tada! Make UI more fancy

--- frontend/src/pages/Chatbot.jsx
+++ frontend/src/pages/Chatbot.jsx
@@ -3,9 +3,19 @@ import { useState } from 'react';
 import { GoDependabot } from "react-icons/go";
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import "./Chatbot.css"
+import robotAvatar from "../assets/robot.svg";
+import user1 from "../assets/usr1.svg"
+import user2 from "../assets/usr2.svg"
+import user3 from "../assets/usr3.svg"
+import user4 from "../assets/usr4.svg"
+import ava1 from "../assets/ava1.svg"
+import ava2 from "../assets/ava2.svg"
+import ava3 from "../assets/ava3.svg"
+import ava4 from "../assets/ava4.svg"
+import ava5 from "../assets/ava5.svg"
 import axios from "axios";
 
-import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from "@chatscope/chat-ui-kit-react"
+import { Avatar, MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from "@chatscope/chat-ui-kit-react"
 
 export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
@@ -39,6 +49,29 @@ export const Chatbot = () => {
         await proccessMessage(allMessages, sanitizedMessage);
     }
 
+    const [avatars_user, setAvatarUsers] = useState([
+        user1
+    ])
+    const [avatar_robot, setAvatarRobots] = useState([
+        ava1
+    ])
+
+    const handlePickAvatars = async () => {
+        // Generate a random index within the range of the array length
+        const options = [user1, user2, user3, user4]
+        const randomIndex = Math.floor(Math.random() * options.length);
+        // Return the avatar at the randomly generated index
+        setAvatarUsers(options[randomIndex]);
+    }
+
+    const handlePickRobots = async () => {
+        // Generate a random index within the range of the array length
+        const options = [ava1, ava2, ava3, ava4, ava5]
+        const randomIndex = Math.floor(Math.random() * options.length);
+        // Return the avatar at the randomly generated index
+        setAvatarRobots(options[randomIndex]);
+    }
+
     const proccessMessage = async (allMessages, chatMessage) => {
 
         const apiReqBody = {
@@ -69,16 +102,37 @@ export const Chatbot = () => {
 
 
     return (
-        <div>
-            hello this is chatbot
-            <GoDependabot size={35} />
+        <div >
+            <div class="robot-title">
+                Hello this is chatbot
+                <GoDependabot size={35} />
+            </div>
             <div>
                 <MainContainer>
                     <ChatContainer>
                         <MessageList typingIndicator={typing ? <TypingIndicator /> : null}>
                             {messages.map((message, i) => {
-                                return <Message key={i} model={message} />
+                                if (message.sender === "user") {
+                                    return <Message key={i} model={message} >
+                                        <Avatar
+                                            name="Robot"
+                                            src={avatars_user}
+                                            status="available"
+                                            onClick={handlePickAvatars}
+                                        />
+                                    </Message>
+                                } else {
+                                    return <Message key={i} model={message} >
+                                        <Avatar
+                                            name="Robot"
+                                            src={avatar_robot}
+                                            status="available"
+                                            onClick={handlePickRobots}
+                                        />
+                                    </Message>
+                                }
                             })}
+
                         </MessageList>
                         <MessageInput placeholder="Type message here" onSend={handleSend} />
                     </ChatContainer>
